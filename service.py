b50be84da667e25138dd81cc30cdfb0570701aa0
roidy <roidy@users.noreply.github.com>
2020-10-24 12:14:14 +0100

    initial commit

---
+++ service.py
@@ -0,0 +1,255 @@
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcvfs
+import os
+import hashlib
+import sqlite3
+
+from PIL import Image, ImageEnhance
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+
+
+OLD_IMAGE = ''
+OLD_COLOR = ''
+
+ADDON = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id')
+ADDON_DATA_PATH = os.path.join(xbmc.translatePath("special://profile/addon_data/%s" % ADDON_ID))
+DATABASE = ADDON_DATA_PATH + "/colors.db"
+
+def log(txt,loglevel=xbmc.LOGDEBUG,force=False):
+    txt = txt.decode('utf-8')
+    message = u'[ %s ] %s' % (ADDON_ID,txt)
+    xbmc.log(msg=message.encode('utf-8'), level=loglevel)
+
+class Main(xbmc.Monitor):
+
+    def __init__(self):
+        self.dbConnection = None
+        self.dbCursor = None
+        self.loadDatabase()
+        self.createTable()
+        self.start()
+
+    def start(self):
+        log('Service started', xbmc.LOGNOTICE)
+
+        while not self.abortRequested():
+            self.getColor()
+
+            self.waitForAbort(0.1)
+
+        try:
+            self.dbConnection.close()
+        except:
+            pass
+
+    def loadDatabase(self):
+        try:
+            if not os.path.exists(ADDON_DATA_PATH):
+                os.makedirs(ADDON_DATA_PATH)
+            self.dbConnection = sqlite3.connect(DATABASE)
+        except Exception as e:
+            log('Database error: Failed to open database file %s - %s' % (DATABASE, e), xbmc.LOGERROR)
+
+        try:
+            self.dbCursor = self.dbConnection.cursor()
+        except Exception as e:
+            log('Database error: Failed to create database cursor - %s' % (e), xbmc.LOGERROR)
+
+        return
+    
+    def createTable(self):
+        try:
+            self.dbCursor.execute("CREATE TABLE IF NOT EXISTS colors (id integer PRIMARY KEY, hash text NOT NULL, color text);")
+        except Exception as e:
+            log('Database error: Failed to create table - %s' % e, xbmc.LOGERROR)
+
+        return
+
+    def lookupColor(self, hashval):
+        try:
+            self.dbCursor.execute("SELECT * FROM colors WHERE hash=?", (hashval,))
+            data = self.dbCursor.fetchall()
+            return data
+        except Exception as e:
+            log('Database error: lookupColor failed %s' % e, xbmc.LOGERROR)
+            pass
+
+        return []
+
+    def storeColor(self, hashval, value):
+        try:
+            self.dbCursor.execute("INSERT INTO colors(hash,color) VALUES(?,?)", (hashval, value))
+            self.dbConnection.commit()
+            return
+        except Exception as e:
+            log('Database error: storeColor failed %s' % e, xbmc.LOGERROR)
+            pass
+
+        return
+
+    def removeColor(self, hashval):
+        try:
+            self.dbCursor.execute("DELETE FROM colors WHERE hash=?", (hashval, ))
+            return
+        except Exception as e:
+            log('Database error: removeColor failed %s' % e, xbmc.LOGERROR)
+            pass
+
+        return
+
+    def getColor(self):
+        global OLD_IMAGE
+
+        # Get image from Kodi Control
+        image = xbmc.getInfoLabel('Control.GetLabel(110000)')
+
+        # If image has not changed then no nothing and return
+        if image == OLD_IMAGE:
+            return
+        OLD_IMAGE = image
+
+        # Get hash value of image
+        hashval = hashlib.md5(image).hexdigest()
+
+        # Check database to see if color already exists
+        val = self.lookupColor(hashval)
+
+        if len(val) == 1:
+            if not val[0][2]:
+                # Blank color found remove it from database and regenerate
+                self.removeColor(hashval)
+                # Generate new color
+                domColor = self.processImage(image, hashval)
+                # Store color in database
+                self.storeColor(hashval, domColor)
+                location="new"
+            # Found color in database
+            domColor = val[0][2]
+            location = "database"
+        else:
+            # Generate new color
+            domColor = self.processImage(image, hashval)
+            # Store color in database
+            self.storeColor(hashval, domColor)
+            location="new"
+
+        # Set window property with color
+        window = xbmcgui.Window(10000)
+        window.setProperty('colorgrab', domColor)
+        window.setProperty('cglocation', location)
+
+    def processImage(self, image, hashval):
+        global OLD_COLOR
+
+        palettedata = [255, 0, 0, 255, 110, 0, 255, 220, 0, 178, 255, 0, 68, 255, 0, 0, 255, 42, 0, 255, 153, 0, 246, 255, 0, 136, 255, 0, 25, 255, 84, 0, 255, 195, 0, 255, 255, 0, 203, 255, 0, 93, 0, 0, 0, 255, 255, 255]
+
+        try:
+            # Open image and resize to make operations faster
+            img = self.openImage(image)
+            img.thumbnail((200, 200))
+
+            # Enhance the saturation of the image
+            enh = ImageEnhance.Color(img)
+            img = enh.enhance(2)
+
+            # Generate a new palette image
+            palImage = Image.new('P',(16,16))
+            palImage.putpalette(palettedata * 16)
+
+            # Quantize the image down to the defined palette
+            finalImg = self.quantize(img, palImage)
+
+            # Count the frequancy of the colors in the final quantized image
+            palette = finalImg.getpalette()
+            colorCounts = sorted(finalImg.getcolors(), reverse=True)
+            colors = list()
+            for i in range(len(colorCounts)):
+                paletteIndex = colorCounts[i][1]
+                domColor = palette[paletteIndex*3:paletteIndex*3+3]
+                colors.append(tuple(domColor))
+
+            # Choose the most used color that isn't black or white
+            for col in colors:
+                if col != (0,0,0) and col != (255,255,255):
+                    outColor = col
+                    break
+
+            # format color to a hex string
+            domColor = '%s%s%s' % (format(outColor[0], '02x'), format(outColor[1], '02x'), format(outColor[2], '02x'))
+            OLD_COLOR = domColor
+
+        except Exception as e:
+            log('Color error: image: %s hash: %s error: %s' % (image, hashval, e), xbmc.LOGERROR)
+
+            # Any errors then just use the old color
+            domColor = OLD_COLOR
+            pass
+
+        return domColor
+
+    # Quantize to palette function
+    # https://stackoverflow.com/questions/29433243/convert-image-to-specific-palette-using-pil-without-dithering
+    def quantize(self, silf, palette):
+        """Convert an RGB or L mode image to use a given P image's palette."""
+
+        silf.load()
+
+        # use palette from reference image
+        palette.load()
+        if palette.mode != "P":
+            raise ValueError("bad mode for palette image")
+        if silf.mode != "RGB" and silf.mode != "L":
+            raise ValueError(
+                "only RGB or L mode images can be quantized to a palette"
+                )
+        im = silf.im.convert("P", 0, palette.im)
+        # the 0 above means turn OFF dithering
+
+        # Later versions of Pillow (4.x) rename _makeself to _new
+        try:
+            return silf._new(im)
+        except AttributeError:
+            return silf._makeself(im)
+
+    # Get cached image fro Kodi
+    # Code taken from script.embuary.helper authored by Sualfred
+    # https://github.com/sualfred/script.embuary.helper/blob/leia/resources/lib/image.py
+    def openImage(self, image):
+        cachedImgPath = urllib.unquote(image.replace('image://', ''))
+        if cachedImgPath.endswith('/'):
+            cachedImgPath = cachedImgPath[:-1]
+
+        cachedFiles = list()
+        for path in [xbmc.getCacheThumbName(cachedImgPath), xbmc.getCacheThumbName(image)]:
+            cachedFiles.append(os.path.join('special://profile/Thumbnails/', path[0], path[:-4] + '.jpg'))
+            cachedFiles.append(os.path.join('special://profile/Thumbnails/', path[0], path[:-4] + '.png'))
+            cachedFiles.append(os.path.join('special://profile/Thumbnails/Video/', path[0], path))
+
+        for i in range(1,4):
+            try:
+                for cache in cachedFiles:
+                    if xbmcvfs.exists(cache):
+                        try:
+                            img = Image.open(xbmc.translatePath(cache))
+                            return img
+
+                        except Exception as error:
+                            log('Image error: Failed to open cached image - %s' % error, xbmc.LOGWARNING)
+
+            except Exception as error:
+                log('Image error: Failed to get image: %s - Error: %s' % (image, error), xbmc.LOGERROR)
+                pass
+
+        return ''
+
+
+
+
+if __name__ == '__main__':
+    Main()
